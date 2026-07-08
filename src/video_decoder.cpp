778a5fac5daf15f0e36166bfa49190faa6affba1
sheltonndlovu625-sketch <sheltonndlovu625@gmail.com>
2026-07-08 22:41:52 +0200

    Update video_decoder.cpp

--- src/video_decoder.cpp
+++ src/video_decoder.cpp
@@ -3,21 +3,23 @@
 
 using namespace godot;
 
-static String resolve_path(String p_path) {
-    if (p_path.begins_with("user://") || p_path.begins_with("res://")) {
-        ProjectSettings *ps = ProjectSettings::get_singleton();
-        if (ps) {
-            return ps->globalize_path(p_path);
+namespace {
+    String resolve_path(String p_path) {
+        if (p_path.begins_with("user://") || p_path.begins_with("res://")) {
+            ProjectSettings *ps = ProjectSettings::get_singleton();
+            if (ps) {
+                return ps->globalize_path(p_path);
+            }
         }
+        return p_path;
     }
-    return p_path;
-}
 
-static void log_av_error(const char *prefix, int errnum) {
-    char errbuf[256];
-    av_strerror(errnum, errbuf, sizeof(errbuf));
-    UtilityFunctions::push_error(String("[VideoDecoder] ") + prefix + ": " + errbuf);
-}
+    void log_av_error(const char *prefix, int errnum) {
+        char errbuf[256];
+        av_strerror(errnum, errbuf, sizeof(errbuf));
+        UtilityFunctions::push_error(String("[VideoDecoder] ") + prefix + ": " + errbuf);
+    }
+} // anonymous namespace
 
 void VideoDecoder::_bind_methods() {
     ClassDB::bind_method(D_METHOD("open", "path"), &VideoDecoder::open);
