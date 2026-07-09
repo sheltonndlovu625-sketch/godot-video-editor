39b271dd165ebaef91bc6e500212b211e5404340
sheltonndlovu625-sketch <sheltonndlovu625@gmail.com>
2026-07-09 21:25:37 +0200

    Update timeline_clip.cpp

--- src/timeline_clip.cpp
+++ src/timeline_clip.cpp
@@ -1,4 +1,3 @@
-
 #include "timeline_clip.h"
 
 using namespace godot;
@@ -7,6 +6,10 @@ void TimelineClip::_bind_methods() {
     ClassDB::bind_method(D_METHOD("set_source_path", "path"), &TimelineClip::set_source_path);
     ClassDB::bind_method(D_METHOD("get_source_path"), &TimelineClip::get_source_path);
     ClassDB::add_property("TimelineClip", PropertyInfo(Variant::STRING, "source_path"), "set_source_path", "get_source_path");
+
+    ClassDB::bind_method(D_METHOD("set_proxy_path", "path"), &TimelineClip::set_proxy_path);
+    ClassDB::bind_method(D_METHOD("get_proxy_path"), &TimelineClip::get_proxy_path);
+    ClassDB::add_property("TimelineClip", PropertyInfo(Variant::STRING, "proxy_path"), "set_proxy_path", "get_proxy_path");
 
     ClassDB::bind_method(D_METHOD("set_timeline_start", "time"), &TimelineClip::set_timeline_start);
     ClassDB::bind_method(D_METHOD("get_timeline_start"), &TimelineClip::get_timeline_start);
@@ -29,54 +32,36 @@ void TimelineClip::_bind_methods() {
 }
 
 TimelineClip::TimelineClip() {}
-
 TimelineClip::~TimelineClip() {}
 
-void TimelineClip::set_source_path(const String &p_path) {
-    source_path = p_path;
-}
+void TimelineClip::set_source_path(const String &p_path) { source_path = p_path; }
+String TimelineClip::get_source_path() const { return source_path; }
 
-String TimelineClip::get_source_path() const {
+void TimelineClip::set_proxy_path(const String &p_path) { proxy_path = p_path; }
+String TimelineClip::get_proxy_path() const { return proxy_path; }
+
+String TimelineClip::get_effective_path(bool p_use_proxy) const {
+    if (p_use_proxy && !proxy_path.is_empty()) {
+        return proxy_path;
+    }
     return source_path;
 }
 
-void TimelineClip::set_timeline_start(double p_time) {
-    timeline_start = p_time;
-}
+void TimelineClip::set_timeline_start(double p_time) { timeline_start = p_time; }
+double TimelineClip::get_timeline_start() const { return timeline_start; }
 
-double TimelineClip::get_timeline_start() const {
-    return timeline_start;
-}
+void TimelineClip::set_source_in_point(double p_time) { source_in_point = p_time; }
+double TimelineClip::get_source_in_point() const { return source_in_point; }
 
-void TimelineClip::set_source_in_point(double p_time) {
-    source_in_point = p_time;
-}
+void TimelineClip::set_source_out_point(double p_time) { source_out_point = p_time; }
+double TimelineClip::get_source_out_point() const { return source_out_point; }
 
-double TimelineClip::get_source_in_point() const {
-    return source_in_point;
-}
-
-void TimelineClip::set_source_out_point(double p_time) {
-    source_out_point = p_time;
-}
-
-double TimelineClip::get_source_out_point() const {
-    return source_out_point;
-}
-
-void TimelineClip::set_playback_speed(double p_speed) {
-    playback_speed = p_speed;
-}
-
-double TimelineClip::get_playback_speed() const {
-    return playback_speed;
-}
+void TimelineClip::set_playback_speed(double p_speed) { playback_speed = p_speed; }
+double TimelineClip::get_playback_speed() const { return playback_speed; }
 
 double TimelineClip::get_duration() const {
     double source_duration = source_out_point - source_in_point;
-    if (source_duration <= 0.0) {
-        return 0.0;
-    }
+    if (source_duration <= 0.0) return 0.0;
     return source_duration / playback_speed;
 }
 
