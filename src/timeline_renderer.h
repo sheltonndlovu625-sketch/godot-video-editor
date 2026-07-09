d5b7b22801162922669d8410d959752945fa77c7
sheltonndlovu625-sketch <sheltonndlovu625@gmail.com>
2026-07-09 02:01:31 +0200

    Update timeline_renderer.h

--- src/timeline_renderer.h
+++ src/timeline_renderer.h
@@ -3,6 +3,7 @@
 
 #include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/classes/image.hpp>
+#include <godot_cpp/classes/os.hpp>
 #include <godot_cpp/variant/packed_float32_array.hpp>
 #include "timeline.h"
 #include "video_encoder.h"
