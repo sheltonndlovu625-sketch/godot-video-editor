f1c71e0d99d1aef8e2340ec2e47e824a5a93241a
sheltonndlovu625-sketch <sheltonndlovu625@gmail.com>
2026-07-09 13:53:30 +0200

    Update timeline_renderer.h

--- src/timeline_renderer.h
+++ src/timeline_renderer.h
@@ -3,6 +3,7 @@
 
 #include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/classes/image.hpp>
+#include <godot_cpp/classes/image_texture.hpp>
 #include <godot_cpp/classes/time.hpp>
 #include <godot_cpp/variant/packed_float32_array.hpp>
 #include "timeline.h"
@@ -17,12 +18,15 @@ class TimelineRenderer : public RefCounted {
 private:
     Ref<Timeline> timeline;
     Dictionary decoders;
+    Dictionary texture_cache;
     double last_render_time = -1.0;
 
     Ref<VideoDecoder> get_decoder(const String &p_path);
     bool _needs_seek(double p_time);
     Ref<Image> composite_frames(const TypedArray<Image> &p_frames, int p_width, int p_height);
+    Ref<Image> composite_frames_fast(const Vector<Ref<Image>> &p_frames, int p_width, int p_height);
     PackedFloat32Array mix_audio(const TypedArray<PackedFloat32Array> &p_buffers);
+    Ref<ImageTexture> get_or_create_texture(const String &p_path, int p_width, int p_height);
 
 protected:
     static void _bind_methods();
@@ -32,6 +36,7 @@ public:
     Ref<Timeline> get_timeline() const;
 
     Ref<Image> render_video_frame(double p_time, int p_width, int p_height);
+    Ref<ImageTexture> render_video_frame_to_texture(double p_time, int p_width, int p_height);
     PackedFloat32Array render_audio(double p_time, int p_num_samples, int p_sample_rate);
     bool export_to_file(const String &p_path, int p_width, int p_height, int p_fps, int p_video_bitrate, int p_sample_rate, int p_audio_bitrate);
     void clear_cache();
