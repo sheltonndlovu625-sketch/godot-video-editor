b7d28d0de6964d94ff478481ce2c5a2e43d131c5
sheltonndlovu625-sketch <sheltonndlovu625@gmail.com>
2026-07-17 14:25:55 +0200

    Update timeline_renderer.h

--- src/timeline_renderer.h
+++ src/timeline_renderer.h
@@ -9,6 +9,7 @@
 #include <godot_cpp/classes/canvas_item_material.hpp>
 #include <godot_cpp/variant/packed_float32_array.hpp>
 #include <godot_cpp/variant/transform2d.hpp>
+#include <godot_cpp/variant/vector2i.hpp>
 #include "timeline.h"
 #include "video_encoder.h"
 #include "video_decoder.h"
@@ -20,6 +21,13 @@ namespace godot {
 
 class TimelineRenderer : public RefCounted {
     GDCLASS(TimelineRenderer, RefCounted)
+
+public:
+    enum AspectRatioMode {
+        ASPECT_FILL,
+        ASPECT_FIT,
+        ASPECT_STRETCH
+    };
 
 private:
     Ref<Timeline> timeline;
@@ -45,6 +53,8 @@ private:
     Ref<CanvasItemMaterial> mat_multiply;
     Ref<CanvasItemMaterial> mat_subtract;
 
+    AspectRatioMode aspect_ratio_mode = ASPECT_FILL;
+
     Ref<VideoDecoder> get_decoder(const String &p_path);
     bool _needs_seek(double p_time);
     Ref<Image> composite_frames(const TypedArray<Image> &p_frames, int p_width, int p_height);
@@ -64,6 +74,16 @@ private:
         const Vector<float> &p_opacities,
         int p_width, int p_height);
 
+    Vector2i _get_decode_size(int p_src_w, int p_src_h, int p_dst_w, int p_dst_h) const;
+    void _cpu_blit_normal(Image *p_dst, const Image *p_src, int p_dx, int p_dy, float p_opacity);
+    void _cpu_blit_add(Image *p_dst, const Image *p_src, int p_dx, int p_dy, float p_opacity);
+    void _cpu_blit_multiply(Image *p_dst, const Image *p_src, int p_dx, int p_dy, float p_opacity);
+    void _cpu_blit_subtract(Image *p_dst, const Image *p_src, int p_dx, int p_dy, float p_opacity);
+    void _cpu_blit(Image *p_dst, const Image *p_src, int p_dx, int p_dy, float p_opacity, int p_blend_mode);
+    Ref<Image> _cpu_render_text_overlay(const Ref<TextOverlay> &p_overlay, int p_canvas_w, int p_canvas_h, double p_time);
+    Ref<Image> _cpu_render_image_overlay(const Ref<ImageOverlay> &p_overlay, int p_canvas_w, int p_canvas_h, double p_time);
+    Ref<Image> _cpu_composite_frame(double p_time, int p_width, int p_height);
+
     Ref<Image> _apply_cpu_effects(const Ref<Image> &p_frame, const TypedArray<VideoEffect> &p_effects, int p_width, int p_height);
 
 protected:
@@ -72,6 +92,9 @@ protected:
 public:
     void set_timeline(const Ref<Timeline> &p_timeline);
     Ref<Timeline> get_timeline() const;
+
+    void set_aspect_ratio_mode(int p_mode);
+    int get_aspect_ratio_mode() const;
 
     Ref<Image> render_video_frame(double p_time, int p_width, int p_height);
     Ref<ImageTexture> render_video_frame_to_texture(double p_time, int p_width, int p_height);
@@ -85,5 +108,7 @@ public:
 };
 
 }
+
+VARIANT_ENUM_CAST(TimelineRenderer::AspectRatioMode);
 
 #endif
