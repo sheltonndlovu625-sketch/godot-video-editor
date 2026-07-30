19cb349988a538995ecccf63ee413b81786c2764
sheltonndlovu625-sketch <sheltonndlovu625@gmail.com>
2026-07-31 01:30:41 +0200

    Update build_ffmpeg_linux.sh

--- scripts/build_ffmpeg_linux.sh
+++ scripts/build_ffmpeg_linux.sh
@@ -54,10 +54,7 @@ COMMON_FLAGS=(
     --enable-encoder=mjpeg
     --enable-encoder=aac
     --enable-encoder=png
-    --enable-gpl
-    --enable-libx264
-    --enable-encoder=libx264
-
+ 
 
     --enable-muxer=mp4
     --enable-muxer=mov
